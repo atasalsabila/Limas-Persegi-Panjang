12449f3b40ee20986cb96b54ed07ed0621050df1
Ata <athayyas@gmail.com>
2018-03-10 19:07:28 +0700

    Initial commit

---
+++ LimasPersegiPanjang.php
@@ -0,0 +1,8 @@
+<?php 
+function VolumeLimasPersegiPanjang($panjang, $lebar, $tinggi) {
+	$Volume = ($panjang * $lebar * $tinggi) / 3;
+	echo $Volume; 
+}
+VolumeLimasPersegiPanjang (12, 10, 18);
+
+ ?>
